7203d8fed4e619eda7cd4458bd83aa37d94c0389
pro5essionalsleeper <elenafilipova_zh24@schoolmath.eu>
2026-02-18 19:59:42 +0200

    Add project files.

---
+++ mini adventure game/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace mini_adventure_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, little player! I'm Elix, and you are currently trapped in a cage inside a castle full of monsters. " +
+                "You have to make your way out. I have left some notes around the castle to help you find the exit. But I have to warn you — BE VERY QUIET!!! " +
+                "The monsters have very bad sight, but their hearing is unmatched. So don't make too much noise. Good luck :)");
+
+            Console.WriteLine("What is your name?");
+            
+
+        }
+    }
+}
---
+++ mini adventure game/character.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_adventure_game
+{
+    internal class character
+    {
+        string name = Console.ReadLine();
+    }
+}
