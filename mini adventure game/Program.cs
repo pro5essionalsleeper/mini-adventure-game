3fcc9b45572c3f73167c10d9c8aa5ed8e6d3358c
pro5essionalsleeper <elenafilipova_zh24@schoolmath.eu>
2026-02-25 17:07:20 +0200

    simple character class - done

--- mini adventure game/Program.cs
+++ mini adventure game/Program.cs
@@ -8,8 +8,14 @@
                 "You have to make your way out. I have left some notes around the castle to help you find the exit. But I have to warn you — BE VERY QUIET!!! " +
                 "The monsters have very bad sight, but their hearing is unmatched. So don't make too much noise. Good luck :)");
 
-            Console.WriteLine("What is your name?");
+            Console.WriteLine();
             
+            Console.WriteLine("BUT FIRST - what is your name?");
+            string name = Console.ReadLine();
+            List<string> inventory = new List<string>(); 
+            Character character = new Character(name, inventory);
+
+            Console.WriteLine($"Welcome, {character.Name}! Let's start your adventure!");
 
         }
     }
--- mini adventure game/character.cs
+++ mini adventure game/character.cs
@@ -1,14 +1,33 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace mini_adventure_game
 {
-    internal class character
+    internal class Character
     {
-        string name = Console.ReadLine();
+        private string name;
+        private List<string> inventory;
+
+        public string Name
+        {
+            get;
+            set;
+        }
+        public List<string> Inventory
+        {             
+            get;
+            set;
+        }
+
+        public Character(string name, List<string> inventory) 
+        { 
+            Name = name;
+            this.Inventory = inventory; 
+        }
     }
 }
